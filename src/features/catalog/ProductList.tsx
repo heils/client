7ba0285d2e22bedd4015c6d335712634cd4306b9
heils <sebasmauro3@gmail.com>
2023-07-08 16:04:48 -0500

    Filters added

--- src/features/catalog/ProductList.tsx
+++ src/features/catalog/ProductList.tsx
@@ -1,16 +1,23 @@
 import { Grid } from "@mui/material";
 import { Product } from "../../app/models/products";
 import ProductCard from "./ProductCard";
+import { useAppSelector } from "../../app/util/configureStore";
+import ProductCardSkeleton from "./ProductCardSkeleton";
 
 interface Props {
   products: Product[];
 }
 export default function ProductList({ products }: Props) {
+  const { productsLoaded } = useAppSelector((state) => state.catalog);
   return (
     <Grid container spacing={4}>
       {products.map((product) => (
-        <Grid item xs={3} key={product.id}>
-          <ProductCard key={product.id} product={product} />
+        <Grid item xs={4} key={product.id}>
+          {!productsLoaded ? (
+            <ProductCardSkeleton />
+          ) : (
+            <ProductCard key={product.id} product={product} />
+          )}
         </Grid>
       ))}
     </Grid>
